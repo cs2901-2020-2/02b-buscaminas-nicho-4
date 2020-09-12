9522aa7f5b25c0875d17331918107ed356985bfb
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2020-09-12 18:23:39 -0500

    Agregada la 2da tabla
    Agregada la tabla visible con "o" que oculta los valores y añadida la lógica de la tabla invisible con los valores.

--- src/main/java/buscaminas.java
+++ src/main/java/buscaminas.java
@@ -9,19 +9,22 @@ public class buscaminas {
     //int tabla [][];
 
     //Crea el arreglo de nxn que representa la tabla
-    public static void creartabla(int[][] tabla, int n){
+    public static void creartablas(int[][] tabla,string[][] tablajuego , int n){
         for (int i = 0; i <= n; i++){
             for (int j = 0; j <= n; j++){
                 tabla[i][j] = 0;
+                tablajuego[i][j] = "o";
             }
         }
         set_minas(tabla, n);
-        imprimirtabla(tabla, n);
+        imprimirtabla(tablajuego,n);
     }
+
 
     //Coloca las minas cambiando 0s por 1s
     public static void set_minas(int[][] tabla, int n){
-        Random rand = new Random();
+        tabla[n][n] = 1;
+        /*Random rand = new Random();
         int cant_minas = rand.nextInt(n+1);
         int rand_x = rand.nextInt(n);
         int rand_y = rand.nextInt(n);
@@ -30,12 +33,12 @@ public class buscaminas {
                 tabla[rand_x][rand_y] = 1;
             else
                 rand_x = rand.nextInt(n);
-            rand_y = rand.nextInt(n);
-        }
+                rand_y = rand.nextInt(n);
+        }*/
     }
 
     //Imprime la tabla del juego rellenada de o
-    public static void imprimirtabla(int[][] tabla, int n){
+    public static void imprimirtabla(string[][] tabla, int n){
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 System.out.println(tabla[i][j]);
@@ -44,23 +47,38 @@ public class buscaminas {
         }
     }
 
+    public static void escoger_casilla(int[][] tabla, string[][] tablajuego,int n, boolean estado){
+        int x = scannerObj.nextInt();
+        int y = scannerObj.nextInt();
+        boolean valid_coord = true;
+        while (x > n || y > n){
+            logger.info("Ingresa coordenadas validas: ");
+            x = scannerObj.nextInt();
+            y = scannerObj.nextInt();
+        }
+        tablajuego[x][y] = tabla[x][y];
+        imprimirtabla(tablajuego,n);
+        if (tabla[x][y] == 1){
+            estado = false;
+        }
+    }
+
+
     public static void main(String[] args){
         boolean estado = true;
         logger.info("Ingresa el tamaño de la tabla: ");
         Scanner scannerObj = new Scanner(System.in);
+
         int n = scannerObj.nextInt();
         int[][] tabla = new int[n][n];
-        creartabla(tabla, n);
+        string[][] tablajuego = new string[n][n];
 
-        while(estado){
-            logger.info("Ingresa las coordenadas: ");
-            int x = scannerObj.nextInt();
-            int y = scannerObj.nextInt();
-            if(tabla[x][y]==1){
-                estado=false;
-            }
-            imprimirtabla(tabla,n);
+        creartablas(tabla, tablajuego, n);
+
+        while (estado=true){
+            escoger_casilla(tabla,tablajuego,n,estado);
         }
+
     }
 
 }
